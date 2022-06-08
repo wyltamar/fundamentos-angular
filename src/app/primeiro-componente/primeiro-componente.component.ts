50b9d7b9a23ba50e19382b434d0d1b0ef0dc3c01
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-06-07 21:58:41 -0300

    Criação do meu primeiro componente

---
+++ src/app/primeiro-componente/primeiro-componente.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-primeiro-componente",
+  template: "<h1>Primeito Componente</h1>",
+  styles: ["h1{color:green;}"]
+})
+export class PrimeiroComponenteComponent{}
