6c4e056053db81b92ebeb0f20b51026e1c5f997c
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-06-07 22:20:39 -0300

    criação do segundo componente

---
+++ src/app/segundo-componente/segundo-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-segundo-componente',
+  templateUrl: './segundo-componente.component.html',
+  styleUrls: ['./segundo-componente.component.css']
+})
+export class SegundoComponenteComponent {
+
+  nome = "Wyltamar";
+
+}
