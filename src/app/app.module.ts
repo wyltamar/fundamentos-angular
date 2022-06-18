7b3dd69946530e5b6e2751a4846f50e693d01549
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-06-18 09:58:27 -0300

    criação do componente pai

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-
 import { FormsModule } from '@angular/forms';
 import { RenderizandoListasComponent } from './renderizando-listas/renderizando-listas.component';
 import { ComponentePersonalizadoComponent } from './componente-personalizado/componente-personalizado.component';
+import { ComponentePaiComponent } from './componente-pai/componente-pai.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { ComponentePersonalizadoComponent } from './componente-personalizado/com
     PipeCostumizadoComponent,
     TwoWayDataBindingComponent,
     RenderizandoListasComponent,
-    ComponentePersonalizadoComponent
+    ComponentePersonalizadoComponent,
+    ComponentePaiComponent
   ],
   imports: [
     BrowserModule,
